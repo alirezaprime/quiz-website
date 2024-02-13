0f68d22d4f311f7d8d7a416e6cdb17cc9961738d
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-02-13 13:16:04 +0330

    done half of project

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        num:1,
+        question:"what does html stand for ?",
+        answer:"C. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multi Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        num:2,
+        question:"what is css stand for ?",
+        answer:"A. cascading style sheet",
+        options:[
+            "A. cascading style sheet",
+            "B. cute style sheet",
+            "C. computer style sheet",
+            "D. codehal style sheet"
+        ]
+    },
+    {
+        num:3,
+        question:"what does php stand for ?",
+        answer:"A. hypertext preprocessor",
+        options:[
+            "A. hypertext preprocessor",
+            "B. hometext programming",
+            "C. hypertext preprogramming",
+            "D. programming hypertext proprocessor"
+        ]
+    },
+    {
+        num:4,
+        question:"what does sql stand for ?",
+        answer:"D. structured query language",
+        options:[
+            "A. structured query language",
+            "B. style query language",
+            "C. science question language",
+            "D. structured query language"
+        ]
+    },
+    {
+        num:5,
+        question:"what does XML stand for ?",
+        answer:"D. extensible markup language",
+        options:[
+            "A. exellent multiple language",
+            "B. explore multiple language",
+            "C. extra markup language",
+            "D. extensible markup language"
+        ]
+    }
+]
