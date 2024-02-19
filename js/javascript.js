7ba37ded90bb146060b376049eeda0f13b7aeb8d
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-02-19 12:47:41 +0330

    i done 2/3 of this project

--- js/javascript.js
+++ js/javascript.js
@@ -22,9 +22,12 @@ continueBtn.onclick = () => {
 
   showQuestions(0);
   questionCounter(1);
+  headerScore();
 };
+
 let questionCount = 0;
 let questionNumb = 1;
+let userScore = 0;
 
 const nextBtn = document.querySelector(".next-btn");
 
@@ -52,10 +55,6 @@ function showQuestions(index) {
   <div class="option"><span>${questions[index].options[1]}</span></div>
   <div class="option"><span>${questions[index].options[2]}</span></div>
   <div class="option"><span>${questions[index].options[3]}</span></div>`;
-  //   let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
-  //   <div class="option"><span>${questions[index].options[1]}</span></div>
-  //   <div class="option"><span>${questions[index].options[2]}</span></div>
-  //   <div class="option"><span>${questions[index].options[3]}</span></div>`;
 
   optionList.innerHTML = optionTag;
 
@@ -67,15 +66,33 @@ function showQuestions(index) {
 function optionSelected(answer) {
   let userAnswer = answer.textContent;
   let correctAnswer = questions[questionCount].answer;
+  let allOption = optionList.children.length;
 
   if (userAnswer == correctAnswer) {
-    console.log('answer is correct');
-    answer.classList.add('correct');
-  }else{
-    answer.classList.add('incorrect');
+    // console.log('answer is correct');
+    answer.classList.add("correct");
+    userScore += 1;
+    headerScore();
+  } else {
+    answer.classList.add("incorrect");
+
+    //if anwer incorrect , auto selected correct answer
+    for (let i = 0; i < allOption; i++) {
+      if (optionList.children[i].textContent == correctAnswer) {
+        optionList.children[i].setAttribute("class", "option correct");
+      }
+    }
+  }
+  // if user selected , disabled all options
+  for (let i = 0; i < allOption; i++) {
+    optionList.children[i].classList.add("disabled");
   }
 }
 function questionCounter(index) {
   const questionTotal = document.querySelector(".question-total");
   questionTotal.textContent = `${index} of ${questions.length} questions`;
+}
+function headerScore() {
+  const headerScoreText = document.querySelector(".header-score");
+  headerScoreText.textContent = `Score ${userScore} / ${questions.length}`;
 }
